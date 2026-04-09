295ccd0bf93e9db6768d1f1f63d5c7d423e0bdca
Anton <Antonpopikhin@gmail.com>
2026-04-09 21:12:32 +0300

    Реализовано задание полностью.

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,27 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2) - 4 * a * c;
+
+  if (d === 0) {
+    let root = -b / (2 * a);
+    arr.push(root);
+
+  } else if (d > 0) {
+    let root1 = (-b + Math.sqrt(d)) / (2 * a);
+    let root2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(root1, root2);
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let p = (percent / 100) / 12;
+  let s = amount - contribution;
+  let monthlyPayment = s * (p + (p / (Math.pow(1 + p, countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  let result = Number(totalAmount.toFixed(2));
+
+  return result;
 }
