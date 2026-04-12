5ac4b8f90e551c5a94a3061c5a9d959d347f35a1
Anton <Antonpopikhin@gmail.com>
2026-04-12 20:50:33 +0300

    Третье задание FIX

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,17 @@
 "use strict";
 
 function compareArrays(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
+	return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let filteredUsers = users.filter(user => user.gender === gender);
+	let filteredUsers = users.filter(user => user.gender === gender);
 
-  if (filteredUsers.length === 0) {
-    return 0;
-  }
+	if (filteredUsers.length === 0) {
+		return 0;
+	}
 
-  let sumOfAges = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+	let sumOfAges = filteredUsers.reduce((acc, user) => acc + user.age, 0);
 
-  return sumOfAges / filteredUsers.length;
+	return sumOfAges / filteredUsers.length;
 }
